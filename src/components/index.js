dc6a2456094824ba68d8072125431b376ac5fcd7
Danail <tdanail12@gmail.com>
2022-04-23 18:24:10 +0300

    Graph Visualiser

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import SortignVisualiser from './SortignVisualiser'
 import PathFindingVisualiser from "./PathFindingVisualiser"
+import SearchingVisualiser from "./SearchingVisualiser"
 
 export { default as Header } from './Header'
 export { default as Overlay } from './Overlay'
@@ -7,8 +8,8 @@ export { default as Player } from './Player'
 export { default as Slider } from './Slider'
 export const Visulisers = {
     sorting: SortignVisualiser,
-    "data-structures": () => <div className=""></div>,
-    "searching": () => <div className=""></div>,
+    searching: SearchingVisualiser,
     "path-finding": PathFindingVisualiser,
+    "data-structures": () => <div className=""></div>,
     undefined: () => <div className=""></div>
 }
