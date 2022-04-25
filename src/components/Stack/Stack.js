c5915b4e7b3a07c076eaab91c511b61081f7bf3f
Danail <tdanail12@gmail.com>
2022-04-25 17:48:50 +0300

    stack

--- src/components/Stack/Stack.js
+++ src/components/Stack/Stack.js
@@ -1,8 +1,37 @@
-import React from "react"
+import React, { useState } from "react"
 import "./styles.scss"
 
-const Stack = ({}) => {
-  return <div className="Stack-container">Stack</div>
+const Stack = () => {
+  const [stack, setStack] = useState([])
+
+  const push = (element) => setStack((stack) => [...stack, element])
+  // const size = () => stack.length
+  const isEmpty = () => stack.length === 0
+  const clear = () => setStack([])
+  const peek = () => stack[stack.length - 1]
+  const pop = () => {
+    if (isEmpty()) return undefined
+    else {
+      const removed = peek()
+      setStack((stack) => stack.slice(0, stack.length - 1))
+      return removed
+    }
+  }
+
+  return <div className="stack-container">
+    <div className="stack-content col">{stack.map(el => <div className="">
+      {el}
+    </div>)}</div>
+    <div className="stack-controls row">
+      <div className="button" onClick={() => {
+        const value = prompt('Enter a number')
+        if (value) push(Number(value))
+      }}>Add</div>
+      <div className="button" onClick={() => alert(peek())}>Peek</div>
+      <div className="button" onClick={pop}>Pop</div>
+      <div className="button" onClick={clear}>Clear</div>
+    </div>
+  </div>
 }
 
 export default Stack
